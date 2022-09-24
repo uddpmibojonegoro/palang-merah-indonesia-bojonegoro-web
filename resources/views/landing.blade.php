e74d17d0e18744f13404a89911639b0d705c0816
anggarapp <anggaraperpe@gmail.com>
2022-09-24 12:22:04 +0700

    hotfix link webgis

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -65,7 +65,7 @@
                                 Donor Darah</a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="https://gis.pmibojonegoro.com"
                                 class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Web
                                 GIS</a>
                         </li>
